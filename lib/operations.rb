f29439829567fedc4bf8f9478fc6a9e8f6a62954
Ghameerah McCullers <mccullers.ghameerah@gmail.com>
2020-05-06 15:00:34 +0000

    Done.

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,11 +1,17 @@
-def unsafe?(speed)
+require 'pry'
 
+def unsafe?(speed)
+  if 60 < speed || speed < 40
+    return true 
+  else 
+    return false
+  end
 end
 
 
 
 def not_safe?(speed)
-	
+	60 < speed || speed < 40 ? true : false
 end
 	
 
