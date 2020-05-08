f86ace445ccefcb2012288985096118c1a1f2c8a
Brian Matheny <bwmatheny@gmail.com>
2020-05-08 18:56:54 +0000

    Done.

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,12 +1,22 @@
-def unsafe?(speed)
+require 'pry'
 
+
+def unsafe?(speed)
+	if speed > 40 || speed < 60
+  false
+  else
+	true
+  end
+  if speed < 40 || speed > 60
+		true
+	else
+		false
+	end
 end
 
 
 
 def not_safe?(speed)
-	
+	speed < 40 || speed > 60 ? true : false
+
 end
-	
-
-
