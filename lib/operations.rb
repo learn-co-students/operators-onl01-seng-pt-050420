3e6f3c3e192a2d6f34e9cb9fee73e886a3c00bf9
jassmine tatum <jassmine.tatum@gmail.com>
2020-05-17 16:50:06 +0000

    Done.

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,12 +1,14 @@
+  def not_safe?(speed)
+  speed < 40 || speed > 60
+end
+
 def unsafe?(speed)
+  if speed > 60 
+    true
+  elsif speed < 40
+    true
+  else
+    false
+  end
 
 end
-
-
-
-def not_safe?(speed)
-	
-end
-	
-
-
